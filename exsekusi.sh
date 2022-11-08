4ad7ddeefebb3428ccf7a2b2d31a769a0cd1dcbc
Diki Andreano <92381721+panglima-jateng@users.noreply.github.com>
2022-11-08 17:46:43 +0700

    Add files via upload

---
+++ exsekusi.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/bash
+# script termux logkont by panglima jateng
+# mode rumit code
+$_jangan_di_recoed_nanti_eror
+$_cie_pusing_yah_liat_codinganku_awokok_
+# code warna yang saya pake
+c='\e[0;36m' # biru muda ( lite )
+lg='\e[1;32m' # hijau ( max )
+dg='\e[1;30m' # abu2
+n='\e[0m' # putih ( lite )
+l='\e[1;31m' # merah
+# sistem isi identitas
+nama=$nameee
+asal=$asli
+umur=$umir
+konfig=sleep
+_panglima=figlet
+gurt=$konfig
+mempersulit=mana
+sipalingelite=$mempersulit
+ajg=$gurt
+echo
+# yakin dek " nanti nangis"
+sleep 1 # > sleep adalah durasi
+echo -e $l" {"$n"•"$l"}"$c" Isi$esal Identitas$excpt Kalian$one Bebas$dut gk maksa"
+k=worldhttps+name+asli+umirwornder-women
+echo -e $lg"••••••••••••••••••••••••••••••••••••••••••••••••••••"$dg
+$konfig 1
+       kita=bersamadowhile$makankucing_groks
+#" >> siapa nama kamu  :" kamu nanya
+makankucing_groks=kamu
+read -p " >> Siapa Nama $makankucing_groks  : " nameee; $https
+$gurt 1
+nangis_dong="fake boleh asli"
+     awal=jangan#diganti$mempersulit
+#" >> kamu orang mana  :" yakin dek
+read -p " >> Kamu Orang $sipalingelite  : " asli; $panglima
+$ajg 1
+    bukan=https://panglima-jateng/database-jv
+$_record_elit_ngoding_sulit
+    kokamujahat=umur
+#" >> kamu umur berapa :" astajim
+read -p " >> Kamu $kokamujahat Berapa : " umir; $jateng
+sleep 3
+    echo
+    oneng=1
+    gila=$oneng
+    echo -e "sedang menyimpan data anda (/)"
+    $konfig $gila
+# data identitas
+echo -e $lg"____________________________________________"$dg
+panglima_jateng=clear
+echo "        DATA ANDA YANG TELAH DI ISI"
+trhreeH=3
+sleep $trhreeH
+nama=$panglima_jateng # donttrew
+echo -e $l" {"$n"•"$l"}"$dg" Nama$neme"$n"  :"$dg" $nameee" # nama ente
+$gurt 3 $_https_panglima$jateng
+headers=panglima.com/googlecrome/shearc # donttrew
+echo -e $l" {"$n"•"$l"}"$dg" Asal$asun"$n"  :"$dg" $asli" # asal ente
+sleep $trhreeH
+team=mafia-teknologi # donttrew
+echo -e $l" {"$n"•"$l"}"$dg" Umur$umin"$n"  :"$dg" $umir" # umur ente
+sleep 3 $_https_panglima$jateng
+gurtt=whatsapp
+$anjay $kelaz   # ente kadang-kadang ente
+echo -e $lg"____________________________________________"
+$konfig $gila
+echo
+prajurit_pajat=//github.com/prajurit-pajat/KlJhBgHyJh-------kjbhfbajjjorr
+dor="terimakasih$selanjutnya anda$akan telah$mencaru mengisi$lengkap data$pribadi anda"
+for i in {1..70}; do
+      echo -ne "\r${dor:0:$i}";
+      sleep 0.05
+# cinta gak selamanya indah dek - dek
+done
+echo
+$nama       # nama kamu siapa?
+$alat $curl # "kamu nanya?" $_index.html
+$kamu $vervid # "sini biar aku kasih tau yh" >> hacked by panglima jateng
+www.panglima_jateng.com(){
+heders=IxAmrmNN5Kk2S75iUtNQkh
+jateng=Sc_Login_2
+dont=lima
+$lima
+work=pang
+$pang
+thre=dak $_https_panglima_jateng_co_id
+asci_art=panglima_jateng # alias jawa tengah
+nnn=san
+gah=fia
+tek=nologi
+the=thor
+echo -e $c"====================================================="
+$_https_panglima_jateng_co_id
+echo -e $l" ["$n"÷"$l"]"$dg" Au$the"$c"  :"$dg" $work$dont jateng"
+$_head # kamu nanya
+dono=mulai # variabel
+echo -e $l" ["$n"÷"$l"]"$dg" Team$nas"$c"    :"$dg" Ma$gah Tek$tek"
+$trunes # kamu bertanya-tanya
+Vnd=git
+echo -e $l" ["$n"÷"$l"]"$dg" Pe$nnn"$c"   :"$dg" Ti$thre Ada Kata Mas"$lah"tah"
+$cruti # rawrr
+echo -e $c"====================================================="
+} # mr virus spm, panglima jateng, prajurit pajat
+# jangan di ganti
+ulang(){
+echo -e $c
+sleep 1
+$_panglima WHATS_KO
+sleep 1
+www.panglima_jateng.com
+sleep 1
+echo
+   echo -e $l"{"$c"•"$l"}"$dg" untuk masuk kedalam script anda harus"$lg" install github"
+   echo -e $c"----------------------------------------------------------"
+   echo -e $l" {"$n" a"$l" }"$dg" install github"
+   echo -e $l" {"$n" b"$l" }"$dg" login to script"
+   echo -e $l" {"$n" c"$l" }"$dg" grup private"
+   echo -e $l" {"$n" d"$l" }"$dg" keluar"
+   echo -e $c"----------------------------------------------------------"
+   echo -e $dg
+   read -p "pilih >> " pill;
+   # database daftar huruf a ( "pertama" )
+   if [[ $pill == "a" ]] || [[ $pill == "A" ]]; then
+   $_AUTHOR_PANGLIMA_JATENG
+   $_TEAM_MAFIA_TEKNOLOGI
+   $_TIDAK_ADA_KATA_MASTAH
+   github="git clone https://github.com/panglima-jateng/m-whatsap"
+   git clone https:$prajurit_pajat
+   fi
+   # database daftar huruf b ( "kedua" )
+   if [[ $pill == "b" ]] || [[ $pill == "B" ]]; then
+   # trio preman mafia teknologi
+   $_hacked_by_mr_virus_spm # legenda mafia teknologi
+   $_hacked_by_panglima_jateng # panglima mafia teknologi
+   $_hacked_by_prajurit_pajat # prajurit mafia teknologi
+   cd KlJhBgHyJh-------kjbhfbajjjorr
+   $_mr_virus_spm_mafia_teknologi
+   $_panglima_jateng_mafia_teknologi
+   $_prajurit_pajat_mafia_teknologi
+   bash kontak.sh
+   fi
+   # database daftar huruf c ( "ketiga" )
+   if [[ $pill == "c" ]] || [[ $pill == "C" ]]; then
+   $_mrvirusspm_panglimajateng_prajuritpajat
+   xdg-open https://chat.$gurtt.com/$heders
+   ulang
+   fi
+   # database daftar huruf d ( "keempat" )
+   if [[ $pill == "d" ]] || [[ $pill == "D" ]]; then
+   # yo ndak tau ko tanya saya
+   dor="terima'kasih telah menggunakan sc saya ✓✓✓"
+	for i in {1..70}; do
+           echo -ne "\r${dor:0:$i}";
+           sleep 0.05
+
+        done
+        echo
+            exit
+            logout
+   fi
+   ulang
+}
+ulang
